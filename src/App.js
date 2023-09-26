2052e129cd1f1780abaa41560ba7f41d8e293cda
LorreaLadao <141934519+LorreaLadao@users.noreply.github.com>
2023-09-26 11:25:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React from 'react';
 import './App.css';
+
+function CalcButton({label}) {
+  return (
+    <button>
+      {label}
+    </button>
+  );
+}
+
+function CalcDisplay({display}) {
+  return (
+    <div className='CalcDisplay'>
+      {display}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="calcu">Welcome to my app</h1>
-      <div className="container">
-        <div className="content">
-          <div className="item-1">0</div>
-          <div className="item-2">7</div>
-          <div className="item-3">8</div>
-          <div className="item-4">9</div>
-          <div className="item-5">+</div>
-          <div className="item-6">4</div>
-          <div className="item-7">5</div>
-          <div className="item-8">6</div>
-          <div className="item-9">-</div>
-          <div className="item-10">1</div>
-          <div className="item-11">2</div>
-          <div className="item-12">3</div>
-          <div className="item-13">*</div>
-          <div className="item-14">C</div>
-          <div className="item-15">0</div>
-          <div className="item-16">=</div>
-          <div className="item-17">÷</div>
-        </div>
-      </div>
-    </div>
+  <div className="CalcContainer"> 
+  <CalcDisplay display={"Hello World!"} />
+    <div className="ButtonContainer"> 
+      < CalcButton label={7}/> 
+      < CalcButton label={8}/> 
+      < CalcButton label={9}/> 
+      < CalcButton label={"+"}/> 
+      < CalcButton label={4}/> 
+      < CalcButton label={5}/> 
+      < CalcButton label={6}/> 
+      < CalcButton label={"-"}/> 
+      < CalcButton label={1}/> 
+      < CalcButton label={2}/> 
+      < CalcButton label={3}/> 
+      < CalcButton label={"*"}/> 
+      < CalcButton label={"C"}/> 
+      < CalcButton label={0}/> 
+      < CalcButton label={"="}/> 
+      < CalcButton label={"÷"}/> 
+    </div> 
+  </div>
   );
 }
 
